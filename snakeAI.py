15dcd5a09371f062cb2951efb5cb358eff440635
hquach-cs <hquach.cs@gmail.com>
2020-02-28 13:03:50 -0600

    Refactor SnakeAI

--- snakeAI.py
+++ snakeAI.py
@@ -2,21 +2,56 @@ import tkinter
 import random
 import time
 import numpy as np
+import math
 
 class NN:
     def __init__(self):
-        self.inputlayercnt = 12
-        self.hiddenlayercnt =  18
-        self.hiddenlayers = 2
-        self.outputlayercnt = 4
-        self.weights = [np.random.uniform(size=(self.inputlayercnt,self.hiddenlayercnt)),np.random.uniform(size=(self.hiddenlayercnt,self.hiddenlayercnt)),np.random.uniform(size=(self.hiddenlayercnt,self.outputlayercnt))]
-        self.bias = [np.random.uniform(size=(1,self.hiddenlayercnt)),np.random.uniform(size=(1,self.hiddenlayercnt)),np.random.uniform(size=(1,self.outputlayercnt))]
-    
-    def feedforward(self,i):
-        hl1 = self.sigmoid(np.dot(i,self.weights[0]) + self.bias[0])
-        hl2 = self.sigmoid(np.dot(hl1,self.weights[1]) + self.bias[1])
-        ol = self.sigmoid(np.dot(hl2,self.weights[2]) + self.bias[2])
-        print(ol)
+        self.generationCnt = 200
+        self.inputNeuronCnt = 8
+        self.hiddenLayerCnt = 2
+        self.hiddenNeuronCnt = 18
+        self.outputNeuronCnt = 4
+        self.fitness = []
+        self.weights = [
+            {
+                'weights': [
+                    np.random.uniform(size=(self.inputNeuronCnt,self.hiddenNeuronCnt)),
+                    #np.random.uniform(size=(self.hiddenNeuronCnt,self.hiddenNeuronCnt)) for _ in range(self.hiddenLayerCnt - 1),
+                    np.random.uniform(size=(self.hiddenNeuronCnt,self.hiddenNeuronCnt)),
+                    np.random.uniform(size=(self.hiddenNeuronCnt,self.outputNeuronCnt))], 
+                'bias':  [
+                    np.random.uniform(size=(1,self.hiddenNeuronCnt)),
+                    np.random.uniform(size=(1,self.hiddenNeuronCnt)),
+                    np.random.uniform(size=(1,self.outputNeuronCnt))]
+            } for _ in range(self.generationCnt)]
+        print(self.weights[0])
+        
+    def newGeneration(self):
+        fitnessSum = sum(self.fitness)
+        self.fitness = [{'fit': x, 'index' : self.fitness.index(x)} for x in self.fitness]
+        self.fitness = sorted(self.fitness, key=lambda fit: fit['fit'],reverse=True)
+        for _ in range(self.generationCnt/2):
+            parent1 = random.randint(0,math.floor(fitnessSum))
+            cnt = 0
+            for i in range(len(self.fitness)):
+                cnt += self.fitness[i]["fit"]
+                if parent1 < cnt:
+                    parent1 = self.fitness[i]
+                    break
+            parent2 = random.randint(0,math.floor(fitnessSum))
+            cnt = 0
+            for i in range(len(self.fitness)):
+                cnt += self.fitness[i]["fit"]
+                if parent2 < cnt:
+                    parent2 = self.fitness[i]
+                    break
+            child1tmp = self.weights[parent1["index"]]
+            child2tmp = self.weights[parent2["index"]]
+
+    def feedforward(self,i,index):
+        hl1 = self.sigmoid(np.dot(i,self.weights[index]["weights"][0]) + self.weights[index]["bias"][0])
+        hl2 = self.sigmoid(np.dot(hl1,self.weights[index]["weights"][1]) + self.weights[index]["bias"][1])
+        ol = self.sigmoid(np.dot(hl2,self.weights[index]["weights"][2]) + self.weights[index]["bias"][2])
         return ol
 
     def sigmoid(self,x):
@@ -26,8 +61,10 @@ class NN:
         # Each steps 10 points is rewarded, Each food 10,000 pts is rewarded, On death -1000 pts
         # 200 Steps is maximum per food count (if over, snake dies) 
         # Maybe later, add TimeCnt so low steps count + food = higher reward
-        return ((steps * 10) + (foods * 10000)) - 1000
-
+        fit = ((steps * 10) + (foods * 10000))
+        self.fitness.append(fit)
+        return
+        
 class cell:
     def __init__(self,x,y):
         self.x = x
@@ -46,98 +83,59 @@ class cell:
         return False
 
 class Snake:
-    def __init__(self,row,col,master):
-        self.nn = NN()
-        self.master = master
+    def __init__(self,row,col):
         self.row = row 
         self.col = col
-        self.Init()
-        self.Board()
-        self.Begin()
-
-    def Board(self):
-        self.canvas = tkinter.Canvas(master,width=420,height=420)
-        self.canvas.pack()
-        self.Draw()
 
     def Move(self,x,y):
-        if self.CheckStatus(x,y):
-            for i in reversed(range(1,len(self.snake))):
-                self.snake[i].x = self.snake[i-1].x 
-                self.snake[i].y = self.snake[i-1].y
-            self.snake[0].x += x 
-            self.snake[0].y += y
+        for i in reversed(range(1,len(self.snake))):
+            self.snake[i].x = self.snake[i-1].x 
+            self.snake[i].y = self.snake[i-1].y
+        self.snake[0].x += x 
+        self.snake[0].y += y
 
 
-    def CheckStatus(self,x,y):
-        x += self.snake[0].x 
-        y += self.snake[0].y
-        snakehead = cell(x,y)
-        if(snakehead.x < 0 or snakehead.x >= row or snakehead.y < 0 or snakehead.y >= col):
-            self.GameOver()
-            return 0
-        if snakehead in self.snake[1:]:
-            self.GameOver()
-            return 0
-        if snakehead == self.food:
+    def CheckStatus(self):
+        if self.snake[0] in self.snake[1:]:
+            self.isAlive = 0
+            return
+        if self.snake[0].x < 0 or self.snake[0].y < 0 or self.snake[0].x >= self.row or self.snake[0].y >= self.col:
+            self.isAlive = 0 
+            return
+        if self.snake[0] == self.food:
             self.score += 1
-            self.snake.append(cell(self.food.x,self.food.y))
-            self.food = cell(random.randint(0,9),random.randint(0,9))
+            self.food = cell(random.randint(0,self.row),random.randint(0,self.col))
         self.steps += 1
-        return 1
-
-    def GameOver(self):
-        ## Calculate Fitness
-        ## Store Data
-        print("Game Over!")
-        self.Init()
-        self.Draw()
 
     def Init(self):
-        self.snake = [cell(random.randint(0,9),random.randint(0,9))]
-        self.food = cell(random.randint(0,9),random.randint(0,9))
+        self.snake = [cell(random.randint(0,self.row),random.randint(0,self.col))]
+        self.food = cell(random.randint(0,self.row),random.randint(0,self.col))
         self.score = 0
         self.steps = 0
+        self.isAlive = 1
 
-    def Draw(self):
-        self.canvas.delete("all")
-        for i in range(0,10):
-            for j in range(0,10):
-                self.canvas.create_rectangle(i * 20+10,j * 20+10,i * 20+30,j * 20+30)
-        for s in self.snake:
-            s.draw(self.canvas,"green")
-        self.food.draw(self.canvas,"red")
-
-    def Begin(self):
-        i = np.random.uniform(size=(1,12))
-        move = list(self.nn.feedforward(i)[0])
-        m = move.index(max(move))
-        if m == 0:
-            self.Move(-1,0)
-        elif m == 1:
-            self.Move(1,0)
-        elif m == 2:
-            self.Move(0,-1)
-        elif m == 3:
-            self.Move(0,1)
-        self.Draw()
-        self.master.after(1000,self.Begin)    
-
-
-if __name__ == "__main__":
-    #### Hyperparameters ####
-    col = 10
-    row = 10
-    #########################
-
-    ####    Globals     #####
-    move = ['l','r','d','u']
-    #########################
-    master = tkinter.Tk()
-    game = Snake(row,col,master)
-
-    tkinter.mainloop()
-
-
-
+    def PlaySnake(self):
+        self.Init()
+        while self.isAlive:
+            move = list(np.random.uniform(size=(1,4)))
+            m = move.index(max(move))
+            if m == 0:
+                self.Move(-1,0)
+            elif m == 1:
+                self.Move(1,0)
+            elif m == 2:
+                self.Move(0,-1)
+            elif m == 3:
+                self.Move(0,1)
+            self.CheckStatus()
+        return self.score, self.steps
     
+game = Snake(10,10)
+score,steps = game.PlaySnake()
+print(score,steps)
+
+#Goals:
+# for index in range(self.generationCnt):
+#     results = playsnake(index)
+#     fitness(results) ## Append results to self.fitness()
+# newGeneration()
